825ab8cb7f3286c9433556caa6d46a0354ff92bc
tejashwinikr <ahwiniteju@gmail.com>
2022-10-07 21:50:14 +0530

    closing 3red section course

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -25,16 +25,7 @@ function App() {
   return (
     <div>
       <h1>DREAMS</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-           title={expenses[1].title}
-           amount={expenses[1].amount}
-           date={expenses[1].date}
-      ></ExpenseItem>
+    <Expenses items={expenses}/>
     </div>
   );
 }
